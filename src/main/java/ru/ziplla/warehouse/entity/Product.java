b34671a6049a1c981b2f74760a30cc3ed091af98
Pavel <pzajtsev@yandex.ru>
2024-06-15 07:38:46 +0400

    create Product entity

--- src/main/java/ru/ziplla/warehouse/entity/Product.java
+++ src/main/java/ru/ziplla/warehouse/entity/Product.java
@@ -1,4 +1,52 @@
 package ru.ziplla.warehouse.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+
+    @NotBlank(message = "Name is required")
+    @Column(name = "name")
+    private String name;
+
+    @NotBlank(message = "Article is required")
+    @Column(name = "article", unique = true, nullable = false)
+    private String article;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "category")
+    private String category;
+
+    @NotNull(message = "Price is required")
+    @PositiveOrZero(message = "Price must be positive or zero")
+    @Column(name = "price")
+    private Double price;
+
+    @PositiveOrZero(message = "Quantity must be positive or zero")
+    @Column(name = "quantity")
+    private Long quantity;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "last_quantity_change", nullable = false)
+    private Date lastQuantityChange;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
 }
